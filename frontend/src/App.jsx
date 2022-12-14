4459be6e14bf74b33a7654787ed7b52f69454917
Vincent CHABOSSON <chabosson@live.fr>
2022-12-14 10:32:47 +0100

    added tailwindCSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
+    <div className="text-3xl text-blue-600 bg-regal-blue lg:w-full">
+      Vincent
     </div>
   );
 }
