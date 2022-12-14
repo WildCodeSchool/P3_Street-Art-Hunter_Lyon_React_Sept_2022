bc44c18d1a1e50047b813fa70b82335ab69d6661
OUASMI <souad@mbp-de-ouasmi.home>
2022-12-14 13:35:36 +0100

    sign in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import BottomNav from "@components/BottomNav";
+import BottomNav from "./components/BottomNav";
+import BlocConnexion from "./components/BlocConnexion";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <BottomNav />
+      <BlocConnexion />
     </div>
   );
 }
