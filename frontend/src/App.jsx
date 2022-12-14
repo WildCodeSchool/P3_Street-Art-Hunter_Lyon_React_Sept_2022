ca0b7b808009dbd9391e6aa7bf0e3628cd7d4e53
Vincent CHABOSSON <chabosson@live.fr>
2022-12-14 17:06:55 +0100

    Switching to scoreboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Stepper from "@pages/Stepper";
+import BottomNav from "@components/BottomNav";
+import ScoreBoard from "@components/ScoreBoard";
 
 function App() {
   return (
     <div>
       <Stepper />
+      <ScoreBoard />
+      <BottomNav />
     </div>
   );
 }
