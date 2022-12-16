c590f66063ef67ac1a6f700204e365a26586a58a
OUASMI <souad@mbp-de-ouasmi.home>
2022-12-16 12:22:45 +0100

    works on  filters

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,4 @@
 import "./App.css";
-// eslint-disable-next-line import/no-unresolved
-import BottomNav from "@components/BottomNav";
-// eslint-disable-next-line import/no-unresolved
-import Stepper from "@pages/Stepper";
 import Connexion from "./pages/Connexion";
 
 function App() {
--- frontend/src/pages/Connexion.jsx
+++ frontend/src/pages/Connexion.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 // eslint-disable-next-line import/no-unresolved
 import Allive from "@components/AllLive";
-import Header from "../components/Header";
 import Filters from "@components/Filters";
-// import Allive from "../components/AllLive";
-// import Filters from "../components/Filters";
-// import SearchBar from "../components/SearchBar";
-// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
-// import BottomNav from "../components/BottomNav";
+import Header from "../components/Header";
 
 function Connexion() {
   return (
