6850c70816264394b91d769b0d8254425d3d3308
Vincent CHABOSSON <chabosson@live.fr>
2022-12-19 09:47:41 +0100

    Making the nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,14 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/stepper" element={<Stepper />} />
       <Route path="/scores" element={<Scores />} />
-      <Route path="menu" element={<Menu />} />
+      <Route path="/menu" element={<Menu />} />
+      <Route path="/help" element={<Home />} />
+      <Route path="/galerie" element={<Home />} />
+      <Route path="/map" element={<Home />} />
+      <Route path="/profil" element={<Home />} />
+      <Route path="/favoris" element={<Home />} />
+      <Route path="/magalerie" element={<Home />} />
+      <Route path="/badges" element={<Home />} />
     </Routes>
   );
 }
