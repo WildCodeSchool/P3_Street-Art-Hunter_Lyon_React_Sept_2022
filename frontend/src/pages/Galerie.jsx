c9407aec02a922bdd4f99f2499fd4439bcfd0696
Vincent CHABOSSON <chabosson@live.fr>
2022-12-20 13:37:43 +0100

    put the cards in galerie

--- frontend/src/pages/Galerie.jsx
+++ frontend/src/pages/Galerie.jsx
@@ -3,13 +3,25 @@ import Allive from "@components/AllLive";
 import BottomNav from "@components/BottomNav";
 import Filters from "@components/Filters";
 import Header from "@components/Header";
+import ArtistCardContainer from "@components/ArtistCardContainer";
 
 export default function Galerie({ allOrLive }) {
   return (
-    <div className="bg-main-background text-white font-main-font bg-cover w-full h-screen">
+    <div className="bg-main-background bg-fixed text-white font-main-font bg-cover w-full h-screen">
       <Header />
       <Allive />
       <Filters allOrLive={allOrLive} />
+      <div className="flex flex-wrap justify-around">
+        <ArtistCardContainer />
+        <ArtistCardContainer />
+        <ArtistCardContainer />
+        <ArtistCardContainer />
+        <ArtistCardContainer />
+        <ArtistCardContainer />
+        <ArtistCardContainer />
+        <ArtistCardContainer />
+      </div>
+
       <BottomNav />
     </div>
   );
