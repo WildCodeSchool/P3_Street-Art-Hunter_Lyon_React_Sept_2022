5422c40c90446bef25dbb27e0fec673586e33e83
gaellegoyon <gaellegoyon.pro@gmail.com>
2023-01-23 14:04:40 +0100

    add vinc

--- frontend/src/components/User/Home/LogoHome.jsx
+++ frontend/src/components/User/Home/LogoHome.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function LogoHome() {
   return (
     <div className="logo-container flex justify-center ">
-      <div className="bg-logo-home bg-cover w-full h-logo flicker-3" />
+      <div className="bg-logo-home bg-cover pl-4 w-full h-logo flicker-3" />
     </div>
   );
 }
