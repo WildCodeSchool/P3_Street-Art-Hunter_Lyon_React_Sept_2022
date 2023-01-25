aa079802ba782a7270b5aee98fc16faec775c4dc
Vincent CHABOSSON <chabosson@live.fr>
2023-01-25 14:40:54 +0100

    made routes protected

--- frontend/src/pages/User/Home.jsx
+++ frontend/src/pages/User/Home.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import LogoHome from "../../components/User/Home/LogoHome";
 import ConnectLandpage from "../../components/User/Home/ConnectLandpage";
+import { useCurrentUserContext } from "../../contexts/userContext";
 
 function Home() {
+  const { user } = useCurrentUserContext();
+  const nav = useNavigate();
+  useEffect(() => {
+    if (user.id) {
+      nav("/camera");
+    }
+  });
+
   return (
     <div className="bg-home-image bg-cover w-full h-screen">
       <LogoHome />
