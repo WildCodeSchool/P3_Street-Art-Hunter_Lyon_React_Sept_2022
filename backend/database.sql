9b5d2582f326bd0f9e198f7bfef9e01833d2ed4e
CODEFORYOU69 <y.ouasmi@gmail.com>
2023-01-25 11:02:16 +0100

    correct db

--- backend/database.sql
+++ backend/database.sql
@@ -1,275 +1,50 @@
--- MySQL Workbench Forward Engineering
+DROP TABLE IF EXISTS artist ;
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS
-, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS
-, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE
-, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE TABLE artist (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  artist_name VARCHAR(45) NULL DEFAULT NULL
+  );
 
-
--- -----------------------------------------------------
--- Table `street_art_hunter`.`artist`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `street_art_hunter`.`artist` ;
-
-CREATE TABLE
-IF NOT EXISTS `street_art_hunter`.`artist`
-(
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `artist_name` VARCHAR
-(45) NULL DEFAULT NULL,
-  PRIMARY KEY
-(`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER
-SET = utf8mb4;
-
-
+INSERT INTO artist (artist_name) VALUES ('Artiste inconnu'),('Vincent'),('Daryl'),('Younes'),('Gaelle');
 -- -----------------------------------------------------
 -- Table `street_art_hunter`.`badge`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `street_art_hunter`.`badge` ;
+DROP TABLE IF EXISTS badge ;
 
-CREATE TABLE
-IF NOT EXISTS `street_art_hunter`.`badge`
-(
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `badge_name` VARCHAR
-(100) NOT NULL,
-  `badge_desc` VARCHAR
-(250) NOT NULL,
-  `badge_img` VARCHAR
-(150) NOT NULL,
-  `badge_condition` VARCHAR
-(200) NOT NULL,
-  PRIMARY KEY
-(`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER
-SET = utf8mb4;
+CREATE TABLE badge (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  badge_name VARCHAR(100) NOT NULL,
+  badge_desc VARCHAR(250) NOT NULL,
+  badge_img VARCHAR(150) NOT NULL,
+  badge_condition VARCHAR(200) NOT NULL
+  );
 
+  INSERT INTO badge VALUES
+  (1, "Médaille d'Or", "Tu as atteint le sommet", "https://i.imgur.com/RGyBBHB.png", "Atteindre le rang 1"),
+  (2, "Médaille d'Argent", "Tu y es presque!", "https://i.imgur.com/RGyBBHB.png", "Atteindre le rang 2"),
+  (3, "Médaille de Bronze", "Tu as atteint le podium", "https://i.imgur.com/RGyBBHB.png", "Atteindre le rang 3"),
+  (4, "Centenaire", "Tu as obtenu 100Points", "https://i.imgur.com/RGyBBHB.png", "Obtenir 10Points"),
+  (5, "Explorateur", "Tu as découvert un terrain inconnu", "https://i.imgur.com/RGyBBHB.png", "Prendre en photo une oeuvre non repertoriée");
 
 -- -----------------------------------------------------
 -- Table `street_art_hunter`.`user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `street_art_hunter`.`user` ;
+DROP TABLE IF EXISTS user ;
 
-CREATE TABLE
-IF NOT EXISTS `street_art_hunter`.`user`
-(
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `firstname` VARCHAR
-(45) NOT NULL,
-  `lastname` VARCHAR
-(45) NOT NULL,
-  `scorepoint` INT NULL DEFAULT '0',
-  `pseudo` VARCHAR
-(45) NOT NULL,
-  `is_admin` TINYINT NULL DEFAULT '0',
-  `email` VARCHAR
-(100) NOT NULL,
-  `hashedPassword` VARCHAR
-(100) NOT NULL,
-  PRIMARY KEY
-(`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 11
-DEFAULT CHARACTER
-SET = utf8mb4;
+CREATE TABLE user (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  firstname VARCHAR(45) NOT NULL,
+  lastname VARCHAR(45) NOT NULL,
+  scorepoint INT NULL DEFAULT '0',
+  pseudo VARCHAR(45) NOT NULL,
+  is_admin TINYINT NULL DEFAULT '0',
+  email VARCHAR(100) NOT NULL,
+  hashedPassword VARCHAR(100) NOT NULL,
+  avatar VARCHAR(150) NULL DEFAULT NULL,
+  passwordtoken VARCHAR(100) NULL DEFAULT NULL
+  );
 
-
-
--- -----------------------------------------------------
--- Table `street_art_hunter`.`work`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `street_art_hunter`.`work` ;
-
-CREATE TABLE
-IF NOT EXISTS `street_art_hunter`.`work`
-(
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `work_name` VARCHAR(150) NOT NULL,
-  `longitude` DECIMAL
-(20,18) NULL DEFAULT NULL,
-  `latitude` DECIMAL
-(20,18) NULL DEFAULT NULL,
-  `value_point` INT NULL DEFAULT NULL,
-  `is_validated` TINYINT NULL DEFAULT NULL,
-  `artist_id` INT NOT NULL,
-  PRIMARY KEY
-(`id`),
-  INDEX `fk_work_artist1_idx`
-(`artist_id` ASC) VISIBLE,
-  CONSTRAINT `fk_work_artist1`
-    FOREIGN KEY
-(`artist_id`)
-    REFERENCES `street_art_hunter`.`artist`
-(`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER
-SET = utf8mb4;
-
-
--- -----------------------------------------------------
--- Table `street_art_hunter`.`picture`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `street_art_hunter`.`picture` ;
-
-CREATE TABLE
-IF NOT EXISTS `street_art_hunter`.`picture`
-(
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `picture_url` VARCHAR(150) NOT NULL,
-  `creation_date` DATETIME NULL DEFAULT NULL,
-  `is_validated` TINYINT NULL DEFAULT NULL,
-  `work_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  PRIMARY KEY
-(`id`),
-  INDEX `fk_picture_work1_idx`
-(`work_id` ASC) VISIBLE,
-  INDEX `fk_picture_user1_idx`
-(`user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_picture_user1`
-    FOREIGN KEY
-(`user_id`)
-    REFERENCES `street_art_hunter`.`user`
-(`id`),
-  CONSTRAINT `fk_picture_work1`
-    FOREIGN KEY
-(`work_id`)
-    REFERENCES `street_art_hunter`.`work`
-(`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER
-SET = utf8mb4;
-
-
--- -----------------------------------------------------
--- Table `street_art_hunter`.`shop`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `street_art_hunter`.`shop` ;
-
-CREATE TABLE
-IF NOT EXISTS `street_art_hunter`.`shop`
-(
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `type` VARCHAR
-(45) NULL DEFAULT NULL,
-  `longitude` DECIMAL
-(10,9) NULL DEFAULT NULL,
-  `latitude` DECIMAL
-(10,9) NULL DEFAULT NULL,
-  `description` VARCHAR
-(45) NULL DEFAULT NULL,
-  PRIMARY KEY
-(`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER
-SET = utf8mb4;
-
-
--- -----------------------------------------------------
--- Table `street_art_hunter`.`user_has_badge`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `street_art_hunter`.`user_has_badge` ;
-
-CREATE TABLE
-IF NOT EXISTS `street_art_hunter`.`user_has_badge`
-(
-  `badge_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  INDEX `fk_user_has_badge_badge1_idx`
-(`badge_id` ASC) VISIBLE,
-  INDEX `fk_user_has_badge_user1_idx`
-(`user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_user_has_badge_badge1`
-    FOREIGN KEY
-(`badge_id`)
-    REFERENCES `street_art_hunter`.`badge`
-(`id`),
-  CONSTRAINT `fk_user_has_badge_user1`
-    FOREIGN KEY
-(`user_id`)
-    REFERENCES `street_art_hunter`.`user`
-(`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER
-SET = utf8mb4;
-
-
--- -----------------------------------------------------
--- Table `street_art_hunter`.`user_has_fav_picture`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `street_art_hunter`.`user_has_fav_picture` ;
-
-CREATE TABLE
-IF NOT EXISTS `street_art_hunter`.`user_has_fav_picture`
-(
-  `user_id` INT NOT NULL,
-  `picture_id` INT NOT NULL,
-  INDEX `fk_user_has_fav_picture_user1_idx`
-(`user_id` ASC) VISIBLE,
-  INDEX `fk_user_has_fav_picture_picture1_idx`
-(`picture_id` ASC) VISIBLE,
-  CONSTRAINT `fk_user_has_fav_picture_picture1`
-    FOREIGN KEY
-(`picture_id`)
-    REFERENCES `street_art_hunter`.`picture`
-(`id`),
-  CONSTRAINT `fk_user_has_fav_picture_user1`
-    FOREIGN KEY
-(`user_id`)
-    REFERENCES `street_art_hunter`.`user`
-(`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER
-SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `street_art_hunter`.`user_has_fav_work`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `street_art_hunter`.`user_has_fav_work` ;
-
-CREATE TABLE
-IF NOT EXISTS `street_art_hunter`.`user_has_fav_work`
-(
-  `user_id` INT NOT NULL,
-  `work_id` INT NOT NULL,
-  INDEX `fk_user_has_fav_work_user1_idx`
-(`user_id` ASC) VISIBLE,
-  INDEX `fk_user_has_fav_work_work1_idx`
-(`work_id` ASC) VISIBLE,
-  CONSTRAINT `fk_user_has_fav_work_user1`
-    FOREIGN KEY
-(`user_id`)
-    REFERENCES `street_art_hunter`.`user`
-(`id`),
-  CONSTRAINT `fk_user_has_fav_work_work1`
-    FOREIGN KEY
-(`work_id`)
-    REFERENCES `street_art_hunter`.`work`
-(`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER
-SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
-SET SQL_MODE
-=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS
-=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS
-=@OLD_UNIQUE_CHECKS;
-
-
-
-INSERT INTO `user`
+  INSERT INTO user (id, firstname, lastname, scorepoint, pseudo, is_admin, email, hashedPassword)
 VALUES
   (1, 'Vincent', 'Chabosson', 1800, 'Gakchabla', 0, 'chabosson@live.fr', '$argon2id$v=19$m=65536,t=5,p=1$+VCtMnhwkGfgDOodFzoG7g$QaC4BxNuJqXzmRMzem8AVgHHssxGLBqx/Vo4MwMaDI0'),
   (2, 'Daryl', 'Chaise', 3000, 'Darylou', 0, '258@joe.fr', '$argon2id$v=19$m=65536,t=5,p=1$vhAxhurqo/BU2H1lKpe7Gg$T30whlZrvP29WtN9IMTSzzo0eynyty53t/7orN3NyZI'),
@@ -282,16 +57,71 @@ VALUES
   (9, 'test', '02', 0, 'Test02', 0, 'toast@live.fr', '$argon2id$v=19$m=65536,t=5,p=1$FjSe7NKP24AgOGHuZQviwA$A8+hy8vJP0pmELqXEoKjBjxDxskKISWlMxoTjbhHBs4'),
   (10, 'Burrito', 'Salsa', 0, 'Taco', 0, 'taco@live.fr', '$argon2id$v=19$m=65536,t=5,p=1$D6WDLZAd3QzAqvC+PNqdjQ$JkcbA54pwnwp1RQoIRj6oZjDG2C+FbUkXDQv6DNd+no');
 
+-- -----------------------------------------------------
+-- Table `street_art_hunter`.`work`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS work ;
 
-insert into badge
-VALUES
-  (1, "Médaille d'Or", "Tu as atteint le sommet", "https://i.imgur.com/RGyBBHB.png", "Atteindre le rang 1"),
-  (2, "Médaille d'Argent", "Tu y es presque!", "https://i.imgur.com/RGyBBHB.png", "Atteindre le rang 2"),
-  (3, "Médaille de Bronze", "Tu as atteint le podium", "https://i.imgur.com/RGyBBHB.png", "Atteindre le rang 3"),
-  (4, "Centenaire", "Tu as obtenu 100Points", "https://i.imgur.com/RGyBBHB.png", "Obtenir 10Points"),
-  (5, "Explorateur", "Tu as découvert un terrain inconnu", "https://i.imgur.com/RGyBBHB.png", "Prendre en photo une oeuvre non repertoriée");
+CREATE TABLE work (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  work_name VARCHAR(150) NOT NULL,
+  longitude DECIMAL(20,18) NULL DEFAULT NULL,
+  latitude DECIMAL(20,18) NULL DEFAULT NULL,
+  value_point INT NULL DEFAULT NULL,
+  is_validated TINYINT NULL DEFAULT NULL,
+  artist_id INT NOT NULL,
+  INDEX fk_work_artist1_idx (artist_id ASC) VISIBLE,
+  CONSTRAINT fk_work_artist1
+  FOREIGN KEY (artist_id) REFERENCES artist(id)
+  ); 
+-- -----------------------------------------------------
+-- Table `street_art_hunter`.`picture`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS picture ;
 
-insert into user_has_badge
+CREATE TABLE picture (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  picture_url VARCHAR(150) NOT NULL,
+  creation_date DATETIME NULL DEFAULT NULL,
+  is_validated TINYINT NULL DEFAULT NULL,
+  work_id INT NOT NULL,
+  user_id INT NOT NULL,
+  INDEX fk_picture_work1_idx (work_id ASC) VISIBLE,
+  INDEX fk_picture_user1_idx (user_id ASC) VISIBLE,
+  CONSTRAINT fk_picture_user1
+  FOREIGN KEY (user_id) REFERENCES user(id),
+  CONSTRAINT fk_picture_work1
+  FOREIGN KEY (work_id) REFERENCES work(id)
+  );
+-- -----------------------------------------------------
+-- Table `street_art_hunter`.`shop`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS shop ;
+
+CREATE TABLE shop (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  shop_type VARCHAR(45) NULL DEFAULT NULL,
+  longitude DECIMAL(20,18) NULL DEFAULT NULL,
+  latitude DECIMAL(20,18) NULL DEFAULT NULL,
+  description VARCHAR(45) NULL DEFAULT NULL
+  );
+
+  -- -----------------------------------------------------
+-- Table `street_art_hunter`.`user_has_badge`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS user_has_badge;
+
+CREATE TABLE user_has_badge
+(
+  badge_id INT NOT NULL,
+  user_id INT NOT NULL,
+   CONSTRAINT fk_user_has_badge_badge1
+  FOREIGN KEY (badge_id) REFERENCES badge(id),
+  CONSTRAINT fk_user_has_badge_user1
+  FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+INSERT INTO user_has_badge
 values
   (1, 1),
   (1, 2),
@@ -304,11 +134,20 @@ values
   (1, 4),
   (4, 4);
 
-ALTER TABLE `street_art_hunter`.`user` 
-ADD COLUMN `avatar` VARCHAR(45) NULL AFTER `hashedPassword`;
 
-INSERT INTO `street_art_hunter`.`artist`
+-- -----------------------------------------------------
+-- Table `street_art_hunter`.`user_has_fav_picture`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS user_has_fav_picture;
+
+CREATE TABLE user_has_fav_picture
 (
-`artist_name`)
-VALUES
-("Artiste Inconnu"),("Vincent"),("Younes"),("Gaëlle"),("Daryl");
+  user_id INT NOT NULL,
+  picture_id INT NOT NULL,
+    FOREIGN KEY
+(user_id) REFERENCES user
+(id),
+  FOREIGN KEY
+(picture_id) REFERENCES picture
+(id)
+ );
