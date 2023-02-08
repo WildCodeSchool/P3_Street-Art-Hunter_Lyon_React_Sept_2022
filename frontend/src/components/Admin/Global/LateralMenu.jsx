7900dee5ae75603694f3654ca54c55e67d373498
ChaiDaryl <chaigne.d01@gmail.com>
2023-02-08 11:19:04 +0100

    modifdermin

--- frontend/src/components/Admin/Global/LateralMenu.jsx
+++ frontend/src/components/Admin/Global/LateralMenu.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import dashboard from "../../../assets/dashboard.svg";
 import user from "../../../assets/user.svg";
-import badge from "../../../assets/badge.svg";
+
 import message from "../../../assets/message.svg";
-import score from "../../../assets/score.svg";
-import camera from "../../../assets/camdash.svg";
-import artwork from "../../../assets/dashartwork.svg";
-import artist from "../../../assets/dashartist.svg";
+import { useCurrentUserContext } from "../../../contexts/userContext";
+
 import shop from "../../../assets/dashshop.svg";
 
 function LateralMenu() {
   const navigate = useNavigate();
+  const { setUser } = useCurrentUserContext();
+
   return (
     <div className="w-[18%] h-screen bg-gradient-to-b from-bottomdark to-[#454377] top-0 fixed">
       <div className="flex justify-center items-start">
@@ -37,44 +37,13 @@ function LateralMenu() {
 
         <div
           aria-hidden="true"
-          onClick={() => navigate("/Admin-Scores")}
-          className="flex justify-start items-center hover:backdrop-blur-sm hover:bg-white/10 pl-8"
-        >
-          <img src={score} className="w-6 h-6" alt="" />
-          <p className="font-main-font p-6 text-2xl text-white">SCORES</p>
-        </div>
-        <div
-          aria-hidden="true"
-          onClick={() => navigate("/Admin-Badges")}
-          className="flex justify-start items-center hover:backdrop-blur-sm hover:bg-white/10 pl-8"
-        >
-          <img src={badge} className="w-6 h-6" alt="" />
-          <p className="font-main-font p-6 text-2xl text-white">BADGES</p>
-        </div>
-        <div
-          aria-hidden="true"
           onClick={() => navigate("/Admin-Messages")}
           className="flex justify-start items-center hover:backdrop-blur-sm hover:bg-white/10 pl-8"
         >
           <img src={message} className="w-6 h-6" alt="" />
           <p className="font-main-font p-6 text-2xl text-white">MESSAGES</p>
         </div>
-        <div
-          aria-hidden="true"
-          onClick={() => navigate("/Admin-Pictures")}
-          className="flex justify-start items-center hover:backdrop-blur-sm hover:bg-white/10 pl-8"
-        >
-          <img src={camera} className="w-6 h-6" alt="" />
-          <p className="font-main-font p-6 text-2xl text-white">PICTURE</p>
-        </div>
-        <div
-          aria-hidden="true"
-          onClick={() => navigate("/Admin-Artist")}
-          className="flex justify-start items-center hover:backdrop-blur-sm hover:bg-white/10 pl-8"
-        >
-          <img src={artist} className="w-6 h-6" alt="" />
-          <p className="font-main-font p-6 text-2xl text-white">ARTIST</p>
-        </div>
+
         <div
           aria-hidden="true"
           onClick={() => navigate("/Admin-Shop")}
@@ -85,12 +54,13 @@ function LateralMenu() {
         </div>
         <div
           aria-hidden="true"
-          onClick={() => navigate("/Admin-Artwork")}
-          className="flex justify-start items-center hover:backdrop-blur-sm hover:bg-white/10 pl-8"
-        >
-          <img src={artwork} className="w-6 h-6" alt="" />
-          <p className="font-main-font p-6 text-2xl text-white">ARTWORK</p>
-        </div>
+          onClick={() => {
+            localStorage.clear();
+            setUser("");
+            navigate("/");
+          }}
+          className="bg-logout w-[12%] h-[4.5vh] bg-contain bg-no-repeat mt-[25rem] ml-[6rem] cursor-pointer"
+        />
       </div>
     </div>
   );
